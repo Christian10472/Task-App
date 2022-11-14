81a17b82bd80be5f10067ad4aca269f9a30130b8
Christian Perez <christian.perez.935@my.csun.edu>
2022-11-13 22:43:35 -0800

    Started work on Database by making new Class. Also cleaned up Task xml layout

--- TaskAppProject/app/src/main/java/com/example/taskappproject/TaskCreationActivity.java
+++ TaskAppProject/app/src/main/java/com/example/taskappproject/TaskCreationActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 
@@ -18,49 +19,57 @@ import java.util.Locale;
 
 public class TaskCreationActivity extends AppCompatActivity {
 
-    //variables for Date Button
-    private DatePickerDialog datePickerDialog;
-    private Button dateButton;
-
-    //variable for Time Button
-    private Button timeButton;
-    int hour;
-    int minuete;
+    //variables
+    EditText et_name;
+    DatePickerDialog datePickerDialog;
+    Button dateButton, timeButton, createButton;
+    int hour, minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_creation);
 
+        //Text reference
+        et_name = findViewById(R.id.et_name);
+
         //Date Button reference
         initDatePicker();
         dateButton = findViewById(R.id.datePickerButton);
-        dateButton.setText(getTodaysDate());
+        dateButton.setText(getTodayDate());
 
         //Time Button reference
         timeButton = findViewById(R.id.timeButton);
 
         //Task type spinner Reference
         Spinner spinner = findViewById(R.id.typeSpinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.taskType, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.taskType, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
         //Task priority spinner Reference
         Spinner prioritySpinner = findViewById(R.id.prioritySpinner);
-        ArrayAdapter<CharSequence> priorityAdapter = ArrayAdapter.createFromResource(this,
-                R.array.taskPriority, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> priorityAdapter = ArrayAdapter.createFromResource(this, R.array.taskPriority, android.R.layout.simple_spinner_item);
         priorityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         prioritySpinner.setAdapter(priorityAdapter);
 
+        //Create Button reference
+        createButton = findViewById(R.id.createButton);
+        /* Button Creation IGNORE FOR NOW
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TaskInformationModel taskInformationModel = new TaskInformationModel(-1, et_name.getText(), );
+            }
+        });
+        */
     }
 
     //Methods for Date Button
-    private String getTodaysDate(){
+    private String getTodayDate(){
         Calendar cal = Calendar.getInstance();
         int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
+        int month = cal.get(Calendar.MONTH) + 1;
         int day = cal.get(Calendar.DAY_OF_MONTH);
         return makeDateString(day, month, year);
     }
@@ -78,11 +87,8 @@ public class TaskCreationActivity extends AppCompatActivity {
         int year = cal.get(Calendar.YEAR);
         int month = cal.get(Calendar.MONTH);
         int day = cal.get(Calendar.DAY_OF_MONTH);
-
         int style = AlertDialog.THEME_HOLO_LIGHT;
-
         datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
-
     }
 
     private String makeDateString(int day, int month, int year) {
@@ -99,14 +105,13 @@ public class TaskCreationActivity extends AppCompatActivity {
             @Override
             public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
                     hour = selectedHour;
-                    minuete = selectedMinute;
-                    timeButton.setText(String.format(Locale.getDefault(), "%02d:%02d", hour, minuete));
+                    minute = selectedMinute;
+                    timeButton.setText(String.format(Locale.getDefault(), "%02d:%02d", hour, minute));
             }
         };
         int style = AlertDialog.THEME_HOLO_DARK;
-        TimePickerDialog timePickerDialog = new TimePickerDialog(this, style, onTimeSetListener, hour, minuete, true);
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, style, onTimeSetListener, hour, minute, true);
         timePickerDialog.setTitle("Select Time");
         timePickerDialog.show();
-
     }
 }
