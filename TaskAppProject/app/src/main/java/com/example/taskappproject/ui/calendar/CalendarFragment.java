c5c5d021b9bbecd5feb1bb85d29d6b1b1e8dc142
Tum J <akkharaw.jomkhanthiphol.179@my.csun.edu>
2022-11-22 11:54:55 -0800

    calendar tab task list (wip)

--- TaskAppProject/app/src/main/java/com/example/taskappproject/ui/calendar/CalendarFragment.java
+++ TaskAppProject/app/src/main/java/com/example/taskappproject/ui/calendar/CalendarFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,8 +16,12 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.ui.AppBarConfiguration;
 
+import com.example.taskappproject.DataBaseHelper;
 import com.example.taskappproject.R;
+import com.example.taskappproject.TaskInformationModel;
 import com.example.taskappproject.databinding.FragmentCalendarBinding;
+
+import java.util.ArrayList;
 
 public class CalendarFragment extends Fragment {
 
@@ -35,6 +42,19 @@ public class CalendarFragment extends Fragment {
 
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         CalendarView calendarview = view.findViewById(R.id.calendarView);
+        calendarview.setOnClickListener(new View.OnClickListener() {
+           public void onClick(View view){
+               ArrayList<TaskInformationModel> tasksDueToday = DataBaseHelper.instance.getTasksDueToday();
+               ListView todaysList = view.findViewById(R.id.TaskView);
+               ArrayList<String> taskNames = new ArrayList<String>();
+               for (int i = 0; i < tasksDueToday.size(); i ++){
+                   taskNames.add(tasksDueToday.get(i).getTaskName());
+               }
+               ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, taskNames);
+               todaysList.setAdapter(adapter);
+               adapter.notifyDataSetChanged();
+           }
+        });
     }
 
     @Override
@@ -42,6 +62,7 @@ public class CalendarFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
 
 
 }
