710d190bf55a73962165401ddad5c0002351e28a
Liam Binell <liambinell@gmail.com>
2022-11-21 23:54:44 -0800

    Replaced instances of "dashboard" with "notes" and updated icon, added new note button (non-functional currently)
    Signed-off-by: Liam Binell <liambinell@gmail.com>

--- TaskAppProject/app/src/main/java/com/example/taskappproject/ui/notes/NotesViewModel.java
+++ TaskAppProject/app/src/main/java/com/example/taskappproject/ui/notes/NotesViewModel.java
@@ -1,16 +1,16 @@
-package com.example.taskappproject.ui.dashboard;
+package com.example.taskappproject.ui.notes;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class NotesViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public NotesViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is notes fragment");
     }
 
     public LiveData<String> getText() {
