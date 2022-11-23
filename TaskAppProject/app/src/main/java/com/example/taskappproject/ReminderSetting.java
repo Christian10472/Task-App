0e4ccce3f33e7127f11d857079c1d93999194248
jaylon111 <jaylon.hartdige.733@my.csun.edu>
2022-11-22 22:00:01 -0800

    Almost done with the design of the reminder setting. I want to add more date and times to repeat.

--- TaskAppProject/app/src/main/java/com/example/taskappproject/ReminderSetting.java
+++ TaskAppProject/app/src/main/java/com/example/taskappproject/ReminderSetting.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 
 public class ReminderSetting extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder_setting);
+
+
     }
 }
